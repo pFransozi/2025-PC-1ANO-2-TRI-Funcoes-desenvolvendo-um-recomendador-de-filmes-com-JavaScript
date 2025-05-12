5fec6047c04cd8c8838894c9e8a5072c9c8003d7
pfransozi <philipehf@gmail.com>
2025-05-11 22:50:36 -0300

    add comment about movie list

--- sketch.js
+++ sketch.js
@@ -1,3 +1,14 @@
+// fantasia, aventura, drama
+
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// as aventuras de pi, 10, drama, fantasia, aventura
+// depois da chuva, 10, drama
+// guardiões da galáxia, 12, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento, 14, drama
+
+
 function setup() {
   createCanvas(400, 400);
 }
