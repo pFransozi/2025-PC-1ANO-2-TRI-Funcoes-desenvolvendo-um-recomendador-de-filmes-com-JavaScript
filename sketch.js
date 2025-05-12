8b8942719f0a3d4ebc3ec83e4acad631cb0643ff
pfransozi <philipehf@gmail.com>
2025-05-11 23:18:15 -0300

    add aula05

--- sketch.js
+++ sketch.js
@@ -10,16 +10,19 @@
 
 function setup() {
     createCanvas(800, 400);
+    createElement("h2", "Recomendador de filmes");
     createSpan("Sua idade:");
     campoIdade = createInput("5");
     campoFantasia = createCheckbox(" Gosta de fantasia?");
+    campoAventura = createCheckbox(" Gosta de aventura?");
 }
 
 function draw() {
     background("white");
     let idade = campoIdade.value();
     let gostaDeFantasia = campoFantasia.checked();
-    let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+    let gostaAventura = campoAventura.checked();
+    let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaAventura);
 
     fill(color(76, 0, 115));
     textAlign(CENTER, CENTER);
@@ -27,22 +30,30 @@ function draw() {
     text(recomendacao, width / 2, height / 2);
 }
 
-function geraRecomendacao(idade, gostaDeFantasia) {
-    if (idade >= 10) {
-        if (idade >= 14) {
-            return "O menino que descobriu o vento";
-        } else {
-            if (gostaDeFantasia) {
-                return "As aventuras de pi";
-            } else {
-                return "Depois da chuva";
-            }
-        }
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
     } else {
-        if (gostaDeFantasia) {
-            return "A viagem de chihiro";
-        } else {
-            return "O feitiço do tempo";
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Ladrões de bicicleta";
         }
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
+        }
+      }
     }
+  } else {
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
+  }
 }
